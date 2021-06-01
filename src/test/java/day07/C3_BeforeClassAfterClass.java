035012f399ced9d247b386b501c19ce9e8f1fa2c
ahmet bulutluoz <abbulutluoz@gmail.com>
2021-06-01 21:54:55 +0300

    v'

--- src/test/java/day07/C3_BeforeClassAfterClass.java
+++ src/test/java/day07/C3_BeforeClassAfterClass.java
@@ -1,27 +1,26 @@
 package day07;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
 
-public class C2_BeforeAfterAnnotations {
+public class C3_BeforeClassAfterClass {
 
-    WebDriver driver;
-    @Before
-    public void setup(){
+    static WebDriver driver;
+
+    @BeforeClass
+    public static void setup(){
         WebDriverManager.chromedriver().setup();
         driver =new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
         System.out.println("setup calisti");
     }
-    @After
-    public void tearDown() throws InterruptedException {
+    @AfterClass
+    public static void tearDown() throws InterruptedException {
         Thread.sleep(5000);
         driver.close();
         System.out.println("teardown calisti");
@@ -48,3 +47,5 @@ public class C2_BeforeAfterAnnotations {
 
 
 }
+
+
